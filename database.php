9bee0c4848385a039cb7e72bffe4faed0cc938e5
k.pawelus <k.pawelus@o2.pl>
2018-02-01 13:29:04 +0100

    added newly built form, no edit or delete option yet

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+	$server = "";
+	$username = "";
+	$password = "";
+	$db = "";
+	
+	$conn = mysqli_connect($server, $username, $password, $db);
+	
+?>
---
+++ process_php.php
@@ -0,0 +1,25 @@
+<?php 
+	include "db4.php";
+	
+	if(isset($_REQUEST['submit_form'])) {
+		echo "The form has been submitted. $_REQUEST[name]";
+	}
+	
+	$sql = "SELECT * FROM learning_ajax";
+	$run = mysqli_query($conn, $sql);
+	$c = 1;
+	while($rows = mysqli_fetch_assoc($run)) {
+		echo "
+			<tr>
+				<td>$c</td>
+				<td>$rows[u_name]</td>
+				<td>$rows[u_email]</td>
+				<td>$rows[u_number]</td>
+				<td>$rows[u_notes]</td>
+				<td><button class='btn btn-success'>Edit</button> <button class='btn btn-danger'>Delete</button></td>
+			</tr>
+		";
+		$c++;
+	}
+?>
+<!-- u_name etc are simply names of the the table columns in your database on server -->
