189fb623cca5ece4e851c334fba42c9d8507b8d8
k.pawelus <k.pawelus@o2.pl>
2018-02-02 20:21:20 +0100

    added delete function

--- process_php.php
+++ process_php.php
@@ -9,18 +9,26 @@
 		$run_sql = mysqli_query($conn, $ins_sql);
 	}
 	
+	if(isset($_REQUEST['del_id'])) {
+		$del_sql = "DELETE FROM users WHERE u_id = '$_REQUEST[del_id]'";
+		$del_run = mysqli_query($conn, $del_sql);
+	}
+	
 	$sql = "SELECT * FROM learning_ajax";
 	$run = mysqli_query($conn, $sql);
 	$c = 1;
 	while($rows = mysqli_fetch_assoc($run)) {
-		echo "
+		echo " 
 			<tr>
 				<td>$c</td>
 				<td>$rows[u_name]</td>
 				<td>$rows[u_email]</td>
 				<td>$rows[u_number]</td>
 				<td>$rows[u_notes]</td>
-				<td><button class='btn btn-success'>Edit</button> <button class='btn btn-danger'>Delete</button></td>
+				<td>
+					<button class='btn btn-success'>Edit</button>
+					<button class='btn btn-danger' onclick=delete_func('$rows[u_id]');>Delete</button>
+				</td>
 			</tr>
 		";
 		$c++;
